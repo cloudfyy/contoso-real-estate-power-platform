19be48a61c635fa13514fa399abf343352dc7336
cloudfyy <cloudfyy@hotmail.com>
2026-07-26 08:07:38 +0800

    Disable MSBuild node reuse for package builds

--- scripts/common/solution-packages.ps1
+++ scripts/common/solution-packages.ps1
@@ -280,7 +280,7 @@ function Invoke-DotNetBuild {
         [string]$ProjectPath
     )
 
-    $arguments = @('build', '-c', 'Release', $ProjectPath)
+    $arguments = @('build', '-c', 'Release', $ProjectPath, '/nodeReuse:false')
 
     Write-Host "dotnet $($arguments -join ' ')" -ForegroundColor Cyan
     & dotnet @arguments
